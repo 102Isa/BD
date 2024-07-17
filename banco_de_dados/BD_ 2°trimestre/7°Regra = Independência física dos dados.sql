4f29907d12cb4ec7ba5904d222cf4f493db322bb
Isabela Magalhaes <isabelagriloribeiromagalhaes@gmail.com>
2024-07-17 08:28:02 -0300

    teste

--- banco_de_dados/BD_ 2°trimestre/7°Regra = Independência física dos dados.sql
+++ banco_de_dados/BD_ 2°trimestre/7°Regra = Independência física dos dados.sql
@@ -4,5 +4,13 @@
 desc funcionario;
 select * from funcionario;
 
+/*Mesmo alterando, por exemplo, o nome de um atributo e o seu tamanho, o comando SQL para relatório consegue refletir a mudança*/
+ALTER TABLE `lanchonete_2d_2024`.`funcionario`
+CHANGE COLUMN `carteiraTrabalho` `carTrab` VARCHAR(90) NOT NULL ;
+
+/*O exemplo seguinte criou 2 atributos da tabela FUNCIONARIO e o relatorio geral continua funcionando...*/
+ALTER TABLE `lanchonete_2d_2024`.`funcionario` 
+ADD COLUMN `email` VARCHAR(250) NULL AFTER `salarioAtual`,
+ADD COLUMN `dataAdmissao` DATE NULL AFTER `email`;
 
 
